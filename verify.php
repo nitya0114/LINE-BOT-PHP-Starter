470f382cc2c477768c676d5cddb025243b83d22a
nitya0114 <41191281+nitya0114@users.noreply.github.com>
2018-07-13 20:21:52 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = '2r+PBXRg1Jps+zXQjyQ7sy8zJSiNTmBNpgL+gj2W2MoquoHLJP7zRZxhL+MZ/mipnH42WCOxgaa/KKTQlLrSvXCmZbLUxmoCqrlvst5rntvn9dfYpbs+LNGcaOuzLeT3DwOTs+lAD4B83gAiZc4yAQdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
