c36460ed3b05a7afbe11e389b824751b5d8a2ccd
nitya0114 <41191281+nitya0114@users.noreply.github.com>
2018-07-13 20:16:42 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = '1oss7tO2qOnlJQE/+Fm0nbVSR1XTm5HZQSNDG7CmaO7AJSNLbpW6OpdXmHDjprR7nH42WCOxgaa/KKTQlLrSvXCmZbLUxmoCqrlvst5rntupQcQX5sqx2gZ4lfsL9xBdoWT23Sg+F2/CncgiYPl8wwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
